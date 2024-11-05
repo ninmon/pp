6e8e03ae9c2d0c8dea54ca0f64f3a32a7a6ff0ab
ninmon <ninmon327@gmail.com>
2024-11-05 12:18:02 +0800

    Initial commit

---
+++ process_tiff_files.py
@@ -0,0 +1,169 @@
+
+import os
+import subprocess
+import argparse
+from pathlib import Path
+import multiprocessing
+import pandas as pd
+import io
+import math
+os.environ['PATH'] += '/home/software/cuda-12.6.2/bin'
+
+ld_library_path = os.environ.get('LD_LIBRARY_PATH', '')
+os.environ['LD_LIBRARY_PATH'] = ld_library_path + ':/home/software/cuda-12.6.2/lib64'
+
+os.environ['PATH'] += ':/usr/local/bin:/home/software/MotionCor2_1.6.4'
+os.environ['PATH'] += ':/usr/local/bin:/home/software/ctffind-5.0.2'
+
+TEMPLATE_BASH_SCRIPT = Path("/home/pp/code/template.sh")
+
+CTFFIND5_COLUMNS = ['Micrograph Number', 'Defocus 1 [Angstroms]', 'Defocus 2 [Angstroms]', 'Azimuth of Astigmatism',
+    'Additional Phase Shift [Radians]', 'Cross Correlation', 'CTF Rings Fit Spacing [Angstroms]',
+    'Estimated Tilt Axis Angle', 'Estimated Tilt Angle', 'Estimated Sample Thickness [Angstroms]']
+
+def read_ctffind5_txt(txt_file):
+    data_lines = []
+    with open(txt_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if not line.startswith('#') and line:  # 跳过以#开头的注释行和空行
+                data_lines.append(line)
+    data = data_lines[-1]
+    df = pd.read_csv(io.StringIO(data), delim_whitespace=True, header=None, names=CTFFIND5_COLUMNS)
+    return df 
+
+def calculate_stigma(defocus_u, defocus_v, stigma_angle, scope, obj_stigma_x=0, obj_stigma_y=0):
+    # Constants from the original Perl script
+    if scope == 2:    
+        x_TEMstigma_step = 38.01
+        y_TEMstigma_step = 36.81
+        y_TEMstigma_angle = -23.61
+        x_TEMstigma_angle = -68.67
+    elif scope == 1:
+        x_TEMstigma_step = 37.01
+        y_TEMstigma_step = 35.58
+        y_TEMstigma_angle = 63.98
+        x_TEMstigma_angle = 19.06   
+      
+    # Calculations for Lx
+    x_angle_diff = x_TEMstigma_angle - stigma_angle
+    kx1 = math.sin(math.radians(x_angle_diff)) / math.cos(math.radians(x_angle_diff))
+    x_angle_diff += 90
+    kx2 = math.sin(math.radians(x_angle_diff)) / math.cos(math.radians(x_angle_diff))
+
+    Lx_sub1 = math.sqrt((kx1 * kx1 + 1) / (defocus_v * defocus_v + defocus_u * defocus_u * kx1 * kx1))
+    Lx_sub2 = math.sqrt((kx2 * kx2 + 1) / (defocus_v * defocus_v + defocus_u * defocus_u * kx2 * kx2))
+    Lx = 0.0001 * defocus_u * defocus_v * (Lx_sub1 - Lx_sub2) / x_TEMstigma_step
+
+    # Calculations for Ly
+    y_angle_diff = y_TEMstigma_angle - stigma_angle
+    ky1 = math.sin(math.radians(y_angle_diff)) / math.cos(math.radians(y_angle_diff))
+    y_angle_diff += 90
+    ky2 = math.sin(math.radians(y_angle_diff)) / math.cos(math.radians(y_angle_diff))
+
+    Ly_sub1 = math.sqrt((ky1 * ky1 + 1) / (defocus_v * defocus_v + defocus_u * defocus_u * ky1 * ky1))
+    Ly_sub2 = math.sqrt((ky2 * ky2 + 1) / (defocus_v * defocus_v + defocus_u * defocus_u * ky2 * ky2))
+    Ly = 0.0001 * defocus_u * defocus_v * (Ly_sub1 - Ly_sub2) / y_TEMstigma_step
+
+    # Calculate new stigma values
+    new_stigma_x = obj_stigma_x + Lx
+    new_stigma_y = obj_stigma_y + Ly
+
+    return "{:.5f}".format(new_stigma_x), "{:.5f}".format(new_stigma_y)
+
+def process_tiff_file(tiff_file, gain_out, motioncor2_dir, ctffind5_dir, stigma_dir, args, frame_num, gpu_id, scope, flag_dir):
+    filename_without_extension = os.path.splitext(os.path.basename(tiff_file))[0]
+    inputfile = os.path.basename(tiff_file)
+    mrc_file = motioncor2_dir / (filename_without_extension + ".mrc")
+    print(os.environ['PATH'])
+    # Run MotionCor2
+    cmd = [
+        "/home/software/MotionCor2_1.6.4/MotionCor2",
+        "-InTiff", str(tiff_file), "-Gain", str(gain_out), "-OutMrc", str(mrc_file),
+        "-FtBin", str(args.binning), "-Patch", f"{args.patch} {args.patch}",
+        "-FmDose", str(args.dose / frame_num), "-PixSize", str(args.pixel_size),
+        "-kV", str(args.accel_kv), "-Gpu", str(gpu_id)
+    ]
+    print(f"Run command: {' '.join(cmd)}")
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+
+    # Run bash script for ctffind5
+    freq_mrc_file = ctffind5_dir / (filename_without_extension + ".mrc")
+    pixel_size_ctf = args.pixel_size * args.binning
+    cmd = [
+        "bash", str(TEMPLATE_BASH_SCRIPT),
+        "-i", str(mrc_file), "-o", str(freq_mrc_file),
+        "-p", str(pixel_size_ctf), "-v", str(args.accel_kv),
+        "-c", str(args.cs_mm), "-a", str(args.amp_contrast),
+        "-s", str(args.spectrum_size), "-rmin", str(args.min_res),
+        "-rmax", str(args.max_res), "-dmin", str(args.min_defocus),
+        "-dmax", str(args.max_defocus), "-step", str(args.defocus_step)
+    ]
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+
+    # Read ctffind5 output and calculate stigma
+    txt_file = ctffind5_dir / (filename_without_extension + ".txt")
+    ctf_params = read_ctffind5_txt(txt_file)
+
+    defocus_u = ctf_params['Defocus 1 [Angstroms]'].values[0]
+    defocus_v = ctf_params['Defocus 2 [Angstroms]'].values[0]
+    stigma_angle = ctf_params['Azimuth of Astigmatism'].values[0]
+    
+    if scope == 1:
+        new_stigma_y, new_stigma_x = calculate_stigma(defocus_u, defocus_v, stigma_angle, scope)
+    elif scope == 2:
+        new_stigma_x, new_stigma_y = calculate_stigma(defocus_u, defocus_v, stigma_angle, scope)
+
+    # Write stigma result to file
+    stigma_file = stigma_dir / f"{txt_file.stem}_stigmaX_{new_stigma_x}_stigmaY_{new_stigma_y}.txt"
+    with open(stigma_file, 'w') as file:
+        file.write(f"# Columns: #1 - new stigma x; #2 - new stigma y\n")
+        file.write(f"{new_stigma_x} {new_stigma_y}\n")
+    #generate done flag
+    flag_file = flag_dir / f"{inputfile}.done"
+    Path(flag_file).touch()
+
+def main(args):
+    tiff_files = args.tiff_files
+    # tiff_files = ["/home/pp/pptest/20241021_wangtian_0500.tif","/home/pp/pptest/20241021_wangtian_0501.tif","/home/pp/pptest/20241021_wangtian_0502.tif","/home/pp/pptest/20241021_wangtian_0503.tif"]
+    gain_out = Path(args.gain_out)
+    motioncor2_dir = Path(args.motioncor2_dir)
+    ctffind5_dir = Path(args.ctffind5_dir)
+    stigma_dir = Path(args.stigma_dir)
+    frame_num = args.frame_num
+    scope = args.scope_id
+    flag_dir = Path(args.flag_dir)
+
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.starmap(
+            process_tiff_file,
+            [(tiff_file, gain_out, motioncor2_dir, ctffind5_dir, stigma_dir, args, frame_num, gpu_id, scope, flag_dir)
+             for tiff_file, gpu_id in zip(tiff_files, range(len(tiff_files)))]
+        )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tiff_files', nargs='+', help='List of tiff files to process', required=True)
+    parser.add_argument('--gain_out', type=str, help='Gain reference output path', required=True)
+    parser.add_argument('--binning', type=int, default=1, help='Binning factor', required=True)
+    parser.add_argument('--patch', type=int, default=5, help='Patch size')
+    parser.add_argument('--dose', type=float, help='Dose per frame', required=True)
+    parser.add_argument('--pixel_size', type=float, help='Pixel size', required=True)
+    parser.add_argument('--accel_kv', type=float, default=300, help='Acceleration voltage in kV')
+    parser.add_argument('--cs_mm', type=float, default=2.7, help='Spherical aberration in mm')
+    parser.add_argument('--amp_contrast', type=float, default=0.07, help='Amplitude contrast')
+    parser.add_argument('--spectrum_size', type=int, default=512, help='Spectrum size')
+    parser.add_argument('--min_res', type=float, default=30.0, help='Minimum resolution')
+    parser.add_argument('--max_res', type=float, default=5.0, help='Maximum resolution')
+    parser.add_argument('--min_defocus', type=float, default=5000.0, help='Minimum defocus')
+    parser.add_argument('--max_defocus', type=float, default=50000.0, help='Maximum defocus')
+    parser.add_argument('--defocus_step', type=float, default=100.0, help='Defocus step')
+    parser.add_argument('--frame_num', type=int, help='Number of frames in the movie', required=True)
+    parser.add_argument('--motioncor2_dir', type=str, help='Directory for MotionCor2 output', required=True)
+    parser.add_argument('--ctffind5_dir', type=str, help='Directory for ctffind5 output', required=True)
+    parser.add_argument('--stigma_dir', type=str, help='Directory for stigma output', required=True)
+    parser.add_argument('--scope_id', type=int, help='BioEM facility microscope number', required=True)
+    parser.add_argument('--flag_dir', type=str, help='Directory for done-flag', required=True)
+
+    args = parser.parse_args()
+    main(args)
---
+++ run_slurm2_with3.py
@@ -0,0 +1,340 @@
+#!/home/software/anaconda3/bin/python3
+import argparse
+import os
+import glob
+import sys
+from pathlib import Path
+import subprocess
+import re
+import io
+import math
+from concurrent.futures import ProcessPoolExecutor
+import time
+import tifffile
+import pwd 
+import grp
+import shutil
+
+MATCH_LIST = ["*.tif", "*.tiff"]
+
+def add_args(parser):
+    
+    # input and output
+    parser.add_argument('--input', type=str, default=None, help='Input directory contains .tif / .tiff')
+    parser.add_argument('--gain', type=str, default=None, help="Gain reference file in dm4 format")
+    parser.add_argument('-o','--output', type=str, default=None, help='Output directory')
+    
+    # optics parameters
+    parser.add_argument("-p", "--pixel_size", type=float, default=1, help="Pixel size in Angstrom")
+    parser.add_argument("-v", "--accel_kv", type=float, default=300.0, help="Accelaration voltage in kV")
+    parser.add_argument("-c", "--cs_mm", type=float, default=2.70, help="Spherical aberration in mm")
+    parser.add_argument("-a", "--amp_contrast", type=float, default=0.07, help="Amplitude contrast")
+    
+    # motioncor2 parameters
+    parser.add_argument("-pat", "--patch", type=int, default=5, help="Patch size")
+    parser.add_argument('-b', "--binning", type=int, default=2, help="Binning factor")
+    parser.add_argument("-d", "--dose", type=float, default=60.0, help="Dose per frame in e/A^2")
+
+    parser.add_argument("-esamp", "--eer_sampling", type=float, default=2, help="EER sampling mode for MotionCor2")
+    parser.add_argument("-efrac", "--eer_fraction", type=float, default=40, help="EER Fractionation for MotionCor2")
+
+    # ctffind5 parameters
+    parser.add_argument("-s", "--spectrum_size", type=int, default=512, help="Size of amplitude spectrum to compute")
+    parser.add_argument("-rmin", "--min_res", type=float, default=30.0, help="Minimum resolution in Angstrom")
+    parser.add_argument("-rmax", "--max_res", type=float, default=5.0, help="Maximum resolution in Angstrom")
+    parser.add_argument("-dmin", "--min_defocus", type=float, default=5000.0, help="Minimum defocus in Angstrom")
+    parser.add_argument("-dmax", "--max_defocus", type=float, default=50000.0, help="Maximum defocus in Angstrom")
+    parser.add_argument("-step", "--defocus_step", type=float, default=100.0, help="Defocus step in Angstrom")
+
+    parser.add_argument("-sc", "--scope_num", type=int, help="BioEM facility microscope number")
+    
+    return parser
+
+def get_tif_frame_count(tif_path):
+    with tifffile.TiffFile(tif_path) as tif:
+        return len(tif.pages)
+
+def recursive_chown_and_acl(path, uid, gid):
+    # 首先更改当前目录的所有权，并为用户添加 ACL 权限
+    os.chown(path, uid, gid)
+    subprocess.run(["setfacl", "-m", f"user:cryosparc-user:rwx", path], check=True)
+
+    # 遍历目录中的所有文件和子目录
+    for root, dirs, files in os.walk(path, topdown=True):
+        # 更改所有子目录的所有权，并为用户添加 ACL 权限
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            os.chown(dir_path, uid, gid)
+            subprocess.run(["setfacl", "-m", f"user:cryosparc-user:rwx", dir_path], check=True)
+        
+        # 更改所有文件的所有权，并为用户添加 ACL 权限
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            os.chown(file_path, uid, gid)
+            subprocess.run(["setfacl", "-m", f"user:cryosparc-user:rwx", file_path], check=True)
+
+def submit_to_slurm(job_script):
+    cmd = ["sudo -u pp sbatch", job_script]
+    ### Submit job to slurm as user "pp" (Running this python script with sudo counts as running it as root, and root cannot submit jobs.)
+    subprocess.run(['sudo', '-u', 'pp', 'sbatch', job_script], check=True)
+
+
+def create_slurm_script(script_path, project_name, tiff_files_chunk, gain_out, args, frame_num, motioncor2_dir, ctffind5_dir, stigma_dir, flag_dir, scope, nums):
+    with open(script_path, 'w') as f:
+        f.write("#!/bin/bash\n")
+        f.write(f"#SBATCH --job-name=T{scope}-{nums}-{project_name}\n")
+        f.write(f"#SBATCH --gres=gpu:4\n")
+        f.write(f"#SBATCH --partition=pp\n")
+        f.write(f"#SBATCH --exclusive\n")
+        f.write(f"#SBATCH --output=/home/pp/out/1.out\n")
+        f.write(f"#SBATCH --error=/home/pp/err/1.err\n")
+        f.write(f"sudo /home/pp/conda/pp-1.0/bin/python /home/pp/code/process_tiff_files.py --tiff_files {' '.join(map(str, tiff_files_chunk))} --gain_out {gain_out} "
+                f"--binning {args.binning} --patch {args.patch} --dose {args.dose} --pixel_size {args.pixel_size} "
+                f"--accel_kv {args.accel_kv} --cs_mm {args.cs_mm} --amp_contrast {args.amp_contrast} --spectrum_size {args.spectrum_size} --eer_fraction {args.eer_fraction} "
+                f"--min_res {args.min_res} --max_res {args.max_res} --min_defocus {args.min_defocus} --max_defocus {args.max_defocus} --flag_dir {flag_dir} --eer_sampling {args.eer_sampling} "
+                f"--defocus_step {args.defocus_step} --frame_num {frame_num} --motioncor2_dir {motioncor2_dir} --ctffind5_dir {ctffind5_dir} --stigma_dir {stigma_dir} --scope_id {scope}\n")
+
+def main(args):
+    ### Get input_dir
+    input_dir = Path(args.input) if args.input is not None else Path(os.getcwd())
+    print(str(input_dir))
+
+
+    ### Attempt to acquire scope_num
+    if args.input is None and args.scope_num is None:
+        if input_dir.parts[2][:5] == "Titan":
+            scope = input_dir.parts[2][5]
+        else:
+            print('Please check current path or specify input path')
+            sys.exit(1)
+    print(scope)
+
+    scope = int(scope)
+
+    if scope == 3:
+        input_dir_data = input_dir / "data"
+    else:
+        input_dir_data = input_dir
+    
+    ### Get project name from input_dir path
+    project_name = os.path.basename(input_dir)
+
+    ### Get and make output_dir. Get user uid & gid from args.output
+    output_dir = Path(args.output) if args.output is not None else input_dir
+    if args.output is not None :
+        username = output_dir.parts[2]
+        try:
+            user_info = pwd.getpwnam(username)
+            uid = user_info.pw_uid
+            gid = user_info.pw_gid
+            print(username,uid,gid)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    ### Chown output_dir if exists in args 
+    if args.output is not None :
+        os.chown(output_dir, uid, gid)   
+    
+    ### Set gain file fullpath from project name 
+    gain_out = str(output_dir / f"{project_name}_gain.mrc")
+
+    ### Convert gain file from dm4 if needed and chown
+    if Path(gain_out).exists():
+        print(f"converted gain file found")
+    elif args.gain is None and scope != 3:
+        dm4_files = glob.glob(os.path.join(input_dir, "*.dm4"))
+        if dm4_files:
+            gain_in = dm4_files[0]
+            
+            # Copy a backup dm4 file to output_dir
+            if args.output is not None:
+                dm4_file_copy = output_dir / os.path.basename(gain_in)
+                shutil.copy(gain_in, dm4_file_copy)  
+
+            #### Trick: Running this script with sudo will clear the evironment, so we must start from source ~/.bashrc
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && dm2mrc {} {}"'.format(gain_in, gain_out), shell=True, check=True)
+        else:
+            print(f"No gain reference file found, please check agian.")
+            sys.exit(1)
+    elif args.gain is None and scope == 3:
+        gain_files = glob.glob(os.path.join(input_dir, "*.gain"))
+        if gain_files:
+            gain_in = gain_files[0]
+            
+            # Copy a backup dm4 file to output_dir
+            if args.output is not None:
+                gain_file_copy = output_dir / os.path.basename(gain_in)
+                shutil.copy(gain_in, gain_file_copy)  
+            gain_out_temp = str(output_dir / f"{project_name}_gain_temp.mrc")
+            #### Trick: Running this script with sudo will clear the evironment, so we must start from source ~/.bashrc
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && tif2mrc {} {}"'.format(gain_in, gain_out_temp), shell=True, check=True)
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && source activate eman2 e2proc2d.py --process math.reciprocal {} {}"'.format(gain_out_temp, gain_out), shell=True, check=True)
+        else:
+            print(f"No gain reference file found, please check agian.")
+            sys.exit(1)
+    else:   # If specified gain file
+        gain = Path(args.gain)
+        gain_in = str(gain)
+
+        # Copy a backup dm4 file to output_dir
+        if args.output is not None:
+            dm4_file_copy = output_dir / os.path.basename(gain_in)
+            shutil.copy(gain_in, dm4_file_copy) 
+
+        if str(gain).endswith('.dm4'):
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && dm2mrc {} {}"'.format(gain_in, gain_out), shell=True, check=True)
+        elif str(gain).endswith('.gain'):
+            gain_out_temp = str(output_dir / f"{project_name}_gain_temp.mrc")
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && tif2mrc {} {}"'.format(gain_in, gain_out_temp), shell=True, check=True)
+            subprocess.run('bash -c "source ~/.bashrc && module load pp && source activate eman2 e2proc2d.py --process math.reciprocal {} {}"'.format(gain_out_temp, gain_out), shell=True, check=True)
+
+        # subprocess.run(cmd, check=True)
+    if args.output is not None :
+        os.chown(gain_out, uid, gid)
+
+    ### Maybe useless
+    assert input_dir.exists() and input_dir.is_dir(), f"Input directory {input_dir} does not exist"
+    # assert gain.exists() and gain.suffix == ".dm4", f"Gain reference file {gain} does not exist"
+
+    #### Prepare directories and chown them
+    motioncor2_dir = output_dir / "motioncor2"
+    motioncor2_dir.mkdir(parents=True, exist_ok=True)
+    ctffind5_dir = output_dir / "ctffind5"
+    ctffind5_dir.mkdir(parents=True, exist_ok=True)
+    stigma_dir = input_dir / "stigma"
+    stigma_dir.mkdir(parents=True, exist_ok=True)
+    script_dir = input_dir / "slurm"
+    script_dir.mkdir(parents=True, exist_ok=True)
+    flag_dir = input_dir / "flag"
+    flag_dir.mkdir(parents=True, exist_ok=True)
+    if args.output is not None :
+        os.chown(flag_dir, uid, gid)
+        os.chown(script_dir, uid, gid)
+        os.chown(stigma_dir, uid, gid)
+        os.chown(ctffind5_dir, uid, gid)
+        os.chown(motioncor2_dir, uid, gid)
+
+    #### Number of files per job submission
+    chunk_size = 4  
+    processed_files = set()
+    timeout = 0
+
+    ### Loop for file scanning
+    while True:
+        ## Scan for tiff files and initialize list by done_flags 
+        tiff_files = list(input_dir_data.glob("*.tif")) + list(input_dir_data.glob("*.tiff")) + list(input_dir_data.glob("*.eer"))
+        tiff_files = sorted(tiff_files)
+        undone_files = []
+        for tiff_file in tiff_files:
+            done_flag = flag_dir / (tiff_file.name + ".done")
+            if not done_flag.exists():  # Only process files without a .done flag
+                undone_files.append(tiff_file)
+
+        ## Filter out already processed files
+        new_tiff_files = [f for f in undone_files if f not in processed_files]
+
+        if len(new_tiff_files) >= chunk_size:
+            tiff_files_chunk = new_tiff_files[:chunk_size]
+
+            # Mark these files as processed
+            processed_files.update(tiff_files_chunk)
+
+            # Get movie shape from the first file
+            if scope == 3:
+                nums = str(tiff_files_chunk[0])[-14:-8] + "-" + str(tiff_files_chunk[-1])[-14:-8]
+                print(nums)
+            else:
+                nums = str(tiff_files_chunk[0])[-8:-4] + "-" + str(tiff_files_chunk[-1])[-8:-4]
+                print(nums)
+            frame_num = get_tif_frame_count(tiff_files_chunk[0])
+
+            Eer_frac_path = motioncor2_dir / "fraction"
+            try:
+                dose_per_frame = args.dose / frame_num
+            except Exception as e:
+                print(e)
+                
+            if not Eer_frac_path.exists() and scope == 3:
+                print("EER fractionation file do not exists!")
+                with open(str(Eer_frac_path), 'w', encoding='utf-8') as file:
+                    # 将变量连接起来并用制表符隔开
+                    line = f"{frame_num}\t{args.eer_fraction}\t{dose_per_frame}\n"
+                    # 将结果写入文件
+                    file.write(line)
+
+            # Create SLURM script and submit job
+            chunk_index = len(processed_files) // chunk_size
+            script_path = script_dir / f"slurm_job_{chunk_index}.sh"
+            create_slurm_script(script_path, project_name, tiff_files_chunk, gain_out, args, frame_num,str(motioncor2_dir),str(ctffind5_dir),str(stigma_dir),str(flag_dir), scope,nums)
+            os.chmod(script_path, 0o755)
+            submit_to_slurm(script_path)
+            
+            if args.output is not None :
+                for tiff_chunk_file in tiff_files_chunk:
+                # 构造目标文件路径
+                    destination = os.path.join(output_dir, os.path.basename(tiff_chunk_file))
+                    shutil.copy2(tiff_chunk_file, destination)
+                    os.chown(destination, uid, gid)
+                        
+            timeout = 0        
+
+            # Wait a bit before scanning again
+            time.sleep(5)  # Adjust the sleep interval as needed
+        
+        elif timeout < 8:
+            timeout += 1
+        
+            time.sleep(5)
+        elif len(new_tiff_files) > 0:
+            tiff_files_chunk = new_tiff_files
+
+            # Mark these files as processed
+            processed_files.update(tiff_files_chunk)
+
+            # Get movie shape from the first file
+            print(str(tiff_files_chunk[0])[-8:-4] + "-" + str(tiff_files_chunk[-1])[-8:-4])
+            frame_num = get_tif_frame_count(tiff_files_chunk[0])
+            
+
+            # Create SLURM script and submit job
+            chunk_index = len(processed_files) // chunk_size
+            script_path = script_dir / f"slurm_job_{chunk_index}.sh"
+            create_slurm_script(script_path, project_name, tiff_files_chunk, gain_out, args, frame_num,str(motioncor2_dir),str(ctffind5_dir),str(stigma_dir),str(flag_dir),scope,nums)
+            os.chmod(script_path, 0o755)
+            submit_to_slurm(script_path)
+
+
+            if args.output is not None :
+                for tiff_chunk_file in tiff_files_chunk:
+                # 构造目标文件路径
+                    destination = os.path.join(output_dir, os.path.basename(tiff_chunk_file))
+                    shutil.copy2(tiff_chunk_file, destination)
+                    os.chown(destination, uid, gid)
+
+            timeout = 0        
+
+            # Wait a bit before scanning again
+            time.sleep(5)  # Adjust the sleep interval as needed
+        else:
+            timeout += 1
+            if timeout % 12 == 0 :
+                print(f"no input, waited {timeout // 12} minute(s)")
+            time.sleep(5)
+        
+        if timeout > 360:
+            print(f"No more input, terminating")
+            if args.output is not None:
+                print("Setting premissions.")
+                recursive_chown_and_acl(output_dir, uid, gid)
+                print(f"premissions set on directory {output_dir}.")
+            sys.exit(1)
+
+
+        
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = add_args(parser).parse_args()
+    main(args)
